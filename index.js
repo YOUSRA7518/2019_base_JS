62b67ee14ab5b671487ca3f6c760a194307e94b3
Yousra <chahi.yousraa@gmail.com>
2019-05-07 14:55:07 +0200

    first

---
+++ index.js
@@ -0,0 +1,8 @@
+var num = 5;
+console.log(typeof num);
+var num = -3;
+console.log(typeof num);
+var string = "chaînes de caractère";
+console.log(typeof string);
+var bfalse = Boolean(false);
+console.log(typeof bfalse);
